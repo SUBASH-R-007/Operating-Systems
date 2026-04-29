de02ff5e6f2ad49e530e97f0cb6d1a1a1e2bdfb4
SUBASH R <subash.r.2237@gmail.com>
2026-04-29 08:40:39 +0530

    added_algo_files

--- ipc.c
+++ ipc.c
@@ -10,27 +10,26 @@
 #define SharedMemSize 50
 void main()
 {
-char c;
-int shmid;
-key_t key;
-char *shared_memory; 
-key = 5677;
-//Create segment with the key specified
-if ((shmid = shmget(key, SharedMemSize, IPC_CREAT |
-0666)) < 0)
-{
-//perror explains error code
-perror("shmget");
-exit(1);
-}
-//Attach the segment
-if((shared_memory= shmat(shmid, NULL, 0)) == (char *) -1) {
-perror("shmat");
-exit(1);
-}
-sprintf(shared_memory," Welcome to Shared Memory");
-sleep(2);
-exit(0);
+    char c;
+    int shmid;
+    key_t key;
+    char *shared_memory; 
+    key = 5677;
+    //Create segment with the key specified
+    if ((shmid = shmget(key, SharedMemSize, IPC_CREAT | 0666)) < 0)
+    {
+        //perror explains error code
+        perror("shmget");
+        exit(1);
+    }
+    //Attach the segment
+    if((shared_memory= shmat(shmid, NULL, 0)) == (char *) -1) {
+        perror("shmat");
+        exit(1);
+    }
+    sprintf(shared_memory," Welcome to Shared Memory");
+    sleep(2);
+    exit(0);
 } 
 
 
@@ -44,25 +43,22 @@ exit(0);
 #define SharedMemSize 50
 void main()
 {
-int shmid;
-key_t key;
-char *shared_memory;
-key = 5677;
-if ((shmid = shmget(key, SharedMemSize, 0666)) < 0) {
-perror("shmget");
-exit(1);
-} 
-
-
-
-//Attach the segment to our data space
-if((shared_memory = shmat(shmid, NULL, 0))==(char *) -1) {
-perror("shmat");
-exit(1);
-}
-//Read the message sender sent to the shared memory
-printf("Message Received: %s \n",shared_memory);
-exit(0);
+    int shmid;
+    key_t key;
+    char *shared_memory;
+    key = 5677;
+    if ((shmid = shmget(key, SharedMemSize, 0666)) < 0) {
+        perror("shmget");
+        exit(1);
+    } 
+    //Attach the segment to our data space
+    if((shared_memory = shmat(shmid, NULL, 0))==(char *) -1) {
+        perror("shmat");
+        exit(1);
+    }
+    //Read the message sender sent to the shared memory
+    printf("Message Received: %s \n",shared_memory);
+    exit(0);
 }
 
 
